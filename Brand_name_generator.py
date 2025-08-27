417a77ae75e93996d284a7178aeca3f46b57b657
pooja <166294526+16ajoop@users.noreply.github.com>
2025-08-27 08:52:33 +0530

    Basic

---
+++ Brand_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Brand Name Generator!")
+city = input("Enter the city you grew up in: ")
+pet = input("Enter name of your favorite pet: ")
+print("Your brand name could be " + city + " " + pet) 
