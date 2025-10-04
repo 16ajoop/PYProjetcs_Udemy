1023ed9e864546ad44016f625c331efaf2f9789f
pooja <166294526+16ajoop@users.noreply.github.com>
2025-10-04 22:03:08 +0530

    Data

---
+++ Day_17/data.py
@@ -0,0 +1,64 @@
+question_data = {
+    "response_code": 0,
+    "results": [
+        {"type": "boolean", "difficulty": "easy",
+         "category": "Entertainment: Music",
+         "question": "In 1993, Prince changed his name to an unpronounceable symbol because he was unhappy with his contract with Warner Bros.",
+         "correct_answer": "True",
+         "incorrect_answers": ["False"]},
+
+        {"type": "boolean", "difficulty": "easy",
+         "category": "Entertainment: Music",
+         "question": "The alternative rock band, They Might Be Giants, released their album &#039;Flood&#039; in 1990. ",
+         "correct_answer": "True",
+         "incorrect_answers": ["False"]},
+
+        {"type": "boolean", "difficulty": "easy",
+         "category": "Entertainment: Music",
+         "question": "American rapper Dr. Dre actually has a Ph.D. doctorate.",
+         "correct_answer": "False",
+         "incorrect_answers": ["True"]},
+
+        {"type": "boolean", "difficulty": "easy",
+         "category": "Entertainment: Music",
+         "question": "A Saxophone is a brass instrument.",
+         "correct_answer": "False",
+         "incorrect_answers": ["True"]},
+
+        {"type": "boolean", "difficulty": "easy",
+         "category": "Entertainment: Music",
+         "question": "Eurobeat is primarily an Italian genre of music.",
+         "correct_answer": "True",
+         "incorrect_answers": ["False"]},
+
+        {"type": "boolean", "difficulty": "easy",
+         "category": "Entertainment: Music",
+         "question": "Daft Punk originated in France.",
+         "correct_answer": "True",
+         "incorrect_answers": ["False"]},
+
+        {"type": "boolean", "difficulty": "easy",
+         "category": "Entertainment: Music",
+         "question": "The 2011 movie &quot;The Adventures of Tintin&quot; was directed by Steven Spielberg.",
+         "correct_answer": "True",
+         "incorrect_answers": ["False"]},
+
+        {"type": "boolean", "difficulty": "easy",
+         "category": "Entertainment: Music",
+         "question": "John Williams composed the music for &quot;Star Wars&quot;.",
+         "correct_answer": "True",
+         "incorrect_answers": ["False"]},
+
+        {"type": "boolean", "difficulty": "easy",
+         "category": "Entertainment: Music",
+         "question": "Korean girl group, TWICE, consists of 9 members",
+         "correct_answer": "True",
+         "incorrect_answers": ["False"]},
+
+        {"type": "boolean", "difficulty": "easy",
+         "category": "Entertainment: Music",
+         "question": "The music video to The Buggle&#039;s &quot;Video Killed the Radio Star&quot; was the first music video to broadcast on MTV.",
+         "correct_answer": "True",
+         "incorrect_answers": ["False"]}
+    ]
+}
