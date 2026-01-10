9fa9af2d7786c96cdd50d10dc63e3cfa2394cdde
pooja <166294526+16ajoop@users.noreply.github.com>
2026-01-10 22:25:16 +0530

    Implement update_scoreboard method for level display
    Refactor scoreboard display to use update_scoreboard method.

--- Day 23/scoreboard.py
+++ Day 23/scoreboard.py
@@ -11,8 +11,18 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.penup()
         self.goto(-280, 250)
-        self.write(f"Level: {self.level}", align="left", font=FONT)
+        self.update_scoreboard()
+
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"level: {self.level}", align="left", font=FONT)
 
     def increase_level(self):
         self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="center", font=FONT)
 
