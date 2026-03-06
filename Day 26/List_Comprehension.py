9da200847cd4c96a11e800384e79b8472355ee93
pooja <166294526+16ajoop@users.noreply.github.com>
2026-03-06 22:17:37 +0530

    Create List_Comprehension.py

---
+++ Day 26/List_Comprehension.py
@@ -0,0 +1,9 @@
+# LIST COMPREHENSION
+
+number = [1,2,3]
+new_numbers = [n + 1 for n in number]
+print(new_numbers)
+
+name  = "Pooja"
+letters_list = [letter for letter in name]
+print(letters_list)
