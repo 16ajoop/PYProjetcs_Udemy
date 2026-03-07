62afda0cb997143136d40137a343acd71df16bd9
pooja <166294526+16ajoop@users.noreply.github.com>
2026-03-07 13:40:48 +0530

    Implement list comprehensions for number operations
    Add list comprehensions for squaring numbers and filtering even numbers.

--- Day 26/List_Comprehension.py
+++ Day 26/List_Comprehension.py
@@ -18,7 +18,25 @@ names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
 short_names = [name.upper() for name in names if len(name) < 5]
 print(short_names)
 
+
+
+
+# 1.
 # SQUARED NUMBERS
+# create a new list called squared_numbers. This new list should contain every number in the list numbers but each number should be squared. 
+# e.g. 4 * 4 = 16
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 squared_numbers = [i*i for i in numbers]
 print(squared_numbers)
+
+
+
+# 2.
+# Filtering Even Numbers
+# First, use list comprehension to convert the list_of_strings to a list of integers called numbers. 
+list_of_strings = ['9', '0', '32', '8', '2', '8', '64', '29', '42', '99']
+numbers = [int(i) for i in list_of_strings]
+print(numbers)
+# Then use list comprehension again to create a new list called result.
+result = [i for i in numbers if i%2==0]
+print(result)
