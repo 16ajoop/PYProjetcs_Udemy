4536ceed52eb18137fe45fd98b0ae1fadfd7ba78
pooja <166294526+16ajoop@users.noreply.github.com>
2026-03-14 18:20:57 +0530

    Update main.py

--- Day 26/Dictionary_Comprehension /main.py
+++ Day 26/Dictionary_Comprehension /main.py
@@ -5,5 +5,7 @@ names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
 students_scores = {student:random.randint(1,100) for student in names}
 print(students_scores)
 
+# passed_student = {new_key:new_value for (key, value) in students_score.items() if test} --------------SYNTAX
+
 passed_student = {student:score for (student,score) in students_scores.items() if score >= 60}
 print(passed_student)
