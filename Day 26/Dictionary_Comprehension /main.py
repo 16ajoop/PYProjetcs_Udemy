b0fc709bc7732929701447c9baaec55728715a53
pooja <166294526+16ajoop@users.noreply.github.com>
2026-03-15 16:48:41 +0530

    working with data frame pandas

--- Day 26/Dictionary_Comprehension /main.py
+++ Day 26/Dictionary_Comprehension /main.py
@@ -24,5 +24,5 @@ import pandas as pd
 student_data_frame = pd.DataFrame(student_dict)
 print(student_data_frame)
 # Loop through rows of a data frame
-for (index, row) in student_data_frame.items():
+for (index, row) in student_data_frame.iterrows():
   print(row)
