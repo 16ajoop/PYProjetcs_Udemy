0cfb1380a113faab680a03db42ef115160c5bb06
pooja <166294526+16ajoop@users.noreply.github.com>
2026-03-15 16:16:16 +0530

    degrees Celsius into degrees Fahrenheit

--- Day 26/Dictionary_Comprehension /exercises.py
+++ Day 26/Dictionary_Comprehension /exercises.py
@@ -4,8 +4,18 @@
 # Try to use Dictionary Comprehension instead of a Loop. 
 
 sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
-
 result = {word: len(word) for word in sentence.split()}
 print(result)
+
+
+# Dictionary Comprehension 2
+# You are going to use Dictionary Comprehension to create a dictionary called weather_f that takes each temperature in degrees Celsius and converts it into degrees Fahrenheit. 
+# To convert temp_c into temp_f use this formula: 
+# (temp_c * 9/5) + 32 = temp_f
+
+weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+weather_f = {day:temp*9/5 + 32 for (day,temp) in weather_c.items()}
+print(weather_f)
+
 
 
